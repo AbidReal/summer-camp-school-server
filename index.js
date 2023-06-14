2a06c2a7388f177a7835f18d6b772ea64dc9481d
Abid Hasan <108365863+AbidReal@users.noreply.github.com>
2023-06-14 18:12:37 +0600

    make instructor done

--- index.js
+++ index.js
@@ -53,12 +53,25 @@ async function run() {
       res.send(result);
     });
 
+    //make admin
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
           role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    //make instructor
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "instructor",
         },
       };
       const result = await usersCollection.updateOne(filter, updateDoc);
