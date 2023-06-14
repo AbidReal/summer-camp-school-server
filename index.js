fddc4b42ee0da7e651765e09d6cfb0bbd85621cb
Abid Hasan <108365863+AbidReal@users.noreply.github.com>
2023-06-15 03:43:06 +0600

    role verify done

--- index.js
+++ index.js
@@ -84,6 +84,34 @@ async function run() {
       res.send(result);
     });
 
+    //get admin
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
+    //get instructor
+    app.get("/users/instructor/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ instructor: false });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === "instructor" };
+      res.send(result);
+    });
+
     //make admin
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
