d407b79015f0d11f4f49007fba62d3ba148149c4
Abid Hasan <108365863+AbidReal@users.noreply.github.com>
2023-06-15 01:43:12 +0600

    jwt installed

--- index.js
+++ index.js
@@ -37,6 +37,15 @@ async function run() {
       .db("schoolMartialArtDB")
       .collection("selectedClasses");
 
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res.send({ token });
+    });
+
     //users data
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
