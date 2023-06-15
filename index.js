e7f819ec2c53e4eca0856e881fb447ff29c69f68
Abid Hasan <108365863+AbidReal@users.noreply.github.com>
2023-06-15 22:23:29 +0600

    server crush fix

--- index.js
+++ index.js
@@ -274,13 +274,13 @@ async function run() {
     });
 
     //create payment intent
-    app.post("/create-payment-intent", async (req, res) => {
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
       const { price } = req.body;
-      const amount = price * 100;
+      const amount = parseInt(price * 100);
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
         currency: "usd",
-        payment_method_types: [card],
+        payment_method_types: ["card"],
       });
       res.send({
         clientSecret: paymentIntent.client_secret,
